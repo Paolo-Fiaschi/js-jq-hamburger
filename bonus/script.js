e97eb29d5cf402964372322e5fbfb1dd39bc3a05
Paolo <p.fiaschi@hotmail.it>
2020-03-31 18:40:52 +0200

    aggiunta eventi

--- bonus/script.js
+++ bonus/script.js
@@ -2,13 +2,16 @@
 var menu = $ (".iconHamburgerMenu");
 var menuHamburger = $ ("#hamburgerMenu");
 var menuClose = $ (".close");
+var candidati = $(".candidati");
+var sconto = $(".sconto");
+
 console.log(menu);
 
 // faccio comparire l'hamburger menu al click
 menu.on({
   click: function(){
     // menuHamburger.addClass('hamburgerMenuClick');
-    menuHamburger.slideDown();
+    menuHamburger.slideDown(600);
     console.log(menu);
   }
 });
@@ -16,8 +19,14 @@ menu.on({
 menuClose.on({
   click: function(){
     // menuHamburger.removeClass('hamburgerMenuClick');
-    menuHamburger.slideUp();
+    menuHamburger.slideUp(600);
 
     console.log(menuClose);
   }
+});
+candidati.mouseover(function() {
+  sconto.show(800);
+});
+candidati.mouseout(function(event) {
+  sconto.hide(800);
 });
